b79c97ff4fc9d7af92603cec2c9c5f82f0214695
Brad Inglis <brad.inglis@gmail.com>
2025-09-12 16:03:41 +1000

    jump changes

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -138,14 +138,6 @@ return {
   {
     "folke/flash.nvim",
     event = "VeryLazy",
-    opts = {
-      modes = {
-        char = {
-          jump_labels = true,
-          label = { exclude = "hjkliardcx" },
-        }
-      }
-    },
     keys = {
       { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
       { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
