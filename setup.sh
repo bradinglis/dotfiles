3f0815b58918dd7ed91d9a90400e0f40b54c61ca
Brad Inglis <brad.inglis@gmail.com>
2025-11-07 17:32:13 +1100

    quiet script

--- setup.sh
+++ setup.sh
@@ -14,56 +14,56 @@ ITALIC='\033[3m'
 UNDERLINE='\033[4m'
 
 cd $HOME
-echo "${BOLD}Adding APT repositories${NONE}"
-sudo add-apt-repository -y ppa:neovim-ppa/unstable
+echo -e "${BOLD}Adding APT repositories${NONE}"
+sudo add-apt-repository -y ppa:neovim-ppa/unstable > /dev/null
 curl -fsSL https://deb.nodesource.com/setup_current.x -o nodesource_setup.sh
 sudo -E bash nodesource_setup.sh
-echo "${BOLD}${GREEN}Complete${NONE}"
-echo ""
+echo -e "${BOLD}${GREEN}Complete${NONE}"
+echo -e ""
 
-echo "${BOLD}Starting APT update/upgrade${NONE}"
+echo -e "${BOLD}Starting APT update/upgrade${NONE}"
 sudo apt-get -y -q update
 sudo apt-get -y -q upgrade
-echo "${BOLD}${GREEN}Complete${NONE}"
-echo ""
+echo -e "${BOLD}${GREEN}Complete${NONE}"
+echo -e ""
 
-echo "${BOLD}Starting APT package downloads${NONE}"
-sudo apt-get -y -q install nodejs fdfind fzf bat less nnn neovim stow zsh git pandoc curl
-echo "${BOLD}${GREEN}Complete${NONE}"
-echo ""
+echo -e "${BOLD}Starting APT package downloads${NONE}"
+sudo apt-get -y -q install nodejs fd-find fzf bat less nnn neovim stow zsh git pandoc curl
+echo -e "${BOLD}${GREEN}Complete${NONE}"
+echo -e ""
 
-echo "${BOLD}Setting up git credential mannager${NONE}"
+echo -e "${BOLD}Setting up git credential mannager${NONE}"
 git config --global credential.helper "/mnt/c/Program\ Files/Git/mingw64/bin/git-credential-manager.exe"
 git config --global credential.https://dev.azure.com.useHttpPath true
-echo "${BOLD}${GREEN}Complete${NONE}"
-echo ""
+echo -e "${BOLD}${GREEN}Complete${NONE}"
+echo -e ""
 
-echo "${BOLD}Installing lazygit${NONE}"
+echo -e "${BOLD}Installing lazygit${NONE}"
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit -D -t /usr/local/bin/
-echo "${BOLD}${GREEN}Complete${NONE}"
-echo ""
+echo -e "${BOLD}${GREEN}Complete${NONE}"
+echo -e ""
  
-echo "${BOLD}Installing oh-my-posh${NONE}"
+echo -e "${BOLD}Installing oh-my-posh${NONE}"
 sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
 sudo chmod +x /usr/local/bin/oh-my-posh
-echo "${BOLD}${GREEN}Complete${NONE}"
-echo ""
+echo -e "${BOLD}${GREEN}Complete${NONE}"
+echo -e ""
 
-echo "${BOLD}Sourcing dotfiles${NONE}"
+echo -e "${BOLD}Sourcing dotfiles${NONE}"
 cd $HOME
 git clone https://github.com/bradinglis/dotfiles.git
 
 cd ./dotfiles
 stow .
-echo "${BOLD}${GREEN}Complete${NONE}"
-echo ""
+echo -e "${BOLD}${GREEN}Complete${NONE}"
+echo -e ""
 
 cd $HOME
 
-echo "${BOLD}Starting Shell${NONE}"
+echo -e "${BOLD}Starting Shell${NONE}"
 chsh -s zsh
 zsh
 
