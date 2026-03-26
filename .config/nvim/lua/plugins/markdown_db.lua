996fee0778e50065cda82fe249c0d4f6444d0826
Brad Inglis <brad.inglis@gmail.com>
2026-03-26 17:01:20 +1100

    config changes with keybinds

--- .config/nvim/lua/plugins/markdown_db.lua
+++ .config/nvim/lua/plugins/markdown_db.lua
@@ -4,17 +4,31 @@ return {
     ft = "markdown",
     opts = {
       vaults = {
-        default = "zettel",
+        default = "zettel_2",
         zettel = {
           name = "zettel",
           dir = "/home/brad/zettel"
-        }
+        },
+        zettel_2 = {
+          name = "zettel_2",
+          dir = "/home/inglisb/testing/zettel"
+        },
+        work = {
+          name = "work",
+          dir = "/home/inglisb/zettel"
+        },
       }
     },
     keys = {
-      { '<leader>ff', function() require("markdown_db-nvim").find_notes() end, mode = 'n', ft = { "markdown" }, desc = 'find notes' },
-      { '<leader>ft', function() require("markdown_db-nvim").find_tags() end, mode = 'n', ft = { "markdown" }, desc = 'find tags' },
-      { '<leader>fr', function() require("markdown_db-nvim").find_links() end, mode = 'n', ft = { "markdown" }, desc = 'find references' },
+      { '<leader>ww', function() require("markdown_db-nvim").find.notes() end,     mode = 'n',        desc = 'find notes' },
+      { '<leader>ff', function() require("markdown_db-nvim").find.notes() end,     mode = 'n',        ft = { "markdown" }, desc = 'find notes' },
+      { '<leader>ft', function() require("markdown_db-nvim").find.tags() end,      mode = 'n',        ft = { "markdown" }, desc = 'find tags' },
+      { '<leader>fr', function() require("markdown_db-nvim").find.links() end,     mode = 'n',        ft = { "markdown" }, desc = 'find references' },
+      { '<leader>h',  function() require("markdown_db-nvim").buf.highlights() end, mode = 'n',        ft = { "markdown" }, desc = 'find references' },
+      { '<CR>',       function() require("markdown_db-nvim").buf.get_cursor() end, mode = 'n',        ft = { "markdown" }, desc = 'cursor' },
+      { '<leader>ns', function() require("markdown_db-nvim").create.source() end,  mode = { 'v', 'n' }, ft = { "markdown" }, desc = 'new source' },
+      { '<leader>na', function() require("markdown_db-nvim").create.author() end,  mode = { 'v', 'n' }, ft = { "markdown" }, desc = 'new source' },
+      { '<leader>nn', function() require("markdown_db-nvim").create.note() end,    mode = { 'v', 'n' }, ft = { "markdown" }, desc = 'new source' },
     },
   }
 }
